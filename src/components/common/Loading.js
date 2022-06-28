c3e84aea53fc64b0a04bf415b335db61f97da4f6
Curtis Updike <cjupdike@icloud.com>
2022-06-28 17:03:00 -0400

    Center Loading spinner

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 
-function Loading() {
-	return (
-		<div
-			style={{
-				marginTop: '40vh',
-				display: 'flex',
-				justifyContent: 'space-around',
-			}}>
-			<Icon icon="spinner" size="3x" pulse />
-		</div>
-	);
-}
+const styles = {
+	container: {
+		position: 'absolute',
+		top: 0,
+		right: 0,
+		bottom: 0,
+		left: 0,
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+};
+
+const Loading = () => (
+	<div style={styles.container}>
+		<Icon icon="spinner" size="3x" pulse />
+	</div>
+);
 
 export default Loading;
