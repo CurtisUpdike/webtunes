ab8a6badc4bbd0aa524d6c1d6d08c930449025ab
Curtis Updike <cjupdike@icloud.com>
2022-07-18 01:52:18 -0400

    Refactor JWT

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const token = require('./functions/getDeveloperToken').handler;
+var jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
@@ -7,8 +7,16 @@ const app = express();
 app.use(express.static('client/build'));
 
 app.get('/api/token', async (req, res) => {
-	const { body } = await token();
-	res.send(body);
+	const developerToken = jwt.sign({}, process.env.AUTH_KEY, {
+		algorithm: 'ES256', // only supported algorithm by MusicKit
+		expiresIn: '30d',
+		issuer: process.env.TEAM_ID,
+		header: {
+			alg: 'ES256',
+			kid: process.env.KEY_ID,
+		},
+	});
+	res.send({ developerToken });
 });
 
 const port = process.env.PORT || 3001;
